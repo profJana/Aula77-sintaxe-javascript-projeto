6b84e08dad5a7f47a54fbf572374aa9058522926
Janaina Tavares <112178884+profJana@users.noreply.github.com>
2023-03-29 23:49:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("Meu primeiro projeto no console");
+
+const primeiroNome = "Janaina";
+const ultimoNome = "Tavares";
+
+const nomeCompleto = primeiroNome.concat(ultimoNome);
+console.log(nomeCompleto);
+
+const nomeCompletov2 = primeiroNome + " " + ultimoNome;
+console.log(nomeCompletov2);
+
+let num1 = 10;
+let num2 = 14;
+
+let soma = num1 + num2;
+console.log(soma);
+
+let sub = num1 - num2;
+console.log(sub);
+
+let mult = num1 * num2;
+console.log(mult);
+
+let div = num1 / num2;
+console.log(div);
